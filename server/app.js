31b83120a7408f8d8e5fbe4c8e2d63794fe854a4
katysobhy89 <katysobhy89@gmail.com>
2024-10-01 22:42:17 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,18 @@
+// server/app.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('templates'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/home.html');
+});
+
+// Add routes for products, cart, checkout, profile, etc.
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
